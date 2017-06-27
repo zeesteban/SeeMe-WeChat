601584328f7d1ce23f7778af57209ffd886a591e
etienne pelletier gagne <etienne.pelletier.gagne0@gmail.com>
2017-06-27 18:42:10 +0800

    created the cards, not styled

--- pages/nearby/nearby.js
+++ pages/nearby/nearby.js
@@ -1,66 +1,30 @@
-// nearby.js
+// // nearby.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    inputShowed: false,
+    inputVal: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   }
-})
+});
+
