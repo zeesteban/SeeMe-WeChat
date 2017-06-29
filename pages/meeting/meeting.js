e2b11e96d1f19571431a905ec4cb08ddeb483e5a
Marketa Dragounova <marketa.dragounova@gmail.com>
2017-06-29 16:15:23 +0800

    messaging

--- pages/meeting/meeting.js
+++ pages/meeting/meeting.js
@@ -1,4 +1,3 @@
-// profile.js
 var app = getApp()
 Page({
   /**
@@ -20,53 +19,22 @@ Page({
       })
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindViewTap: function () {
+    console.log("below is the auth token")
+    console.log(app.globalData.authToken)
+    wx.request({
+      url: 'http://localhost:3000/api/v1/meetings/3/messages', //仅为示例，并非真实的接口地址
+      method: 'post',
+      data: {
+        content: 'hello'
+      },
+      header: {
+        'content-type': 'application/json',
+        'X-User-Token': app.globalData.authToken
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
   }
 })
