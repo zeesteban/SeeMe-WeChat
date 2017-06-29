2ed194a30cbed5d28042b278cb68a7149425b841
Marketa Dragounova <marketa.dragounova@gmail.com>
2017-06-29 18:59:12 +0800

    messaging

--- pages/nearby/nearby.js
+++ pages/nearby/nearby.js
@@ -10,7 +10,7 @@ Page({
     let page = this;
     // Nearby API request
     // wx.request({
-    //   url: 'https://seeme.shanghaiwogeng.com/api/v1/users',
+    //   url: 'http://localhost:3000/api/v1/users',
     //   method: "get",
     //     header: {
     //       'content-type': 'application/json'
